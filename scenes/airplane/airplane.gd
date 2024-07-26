31153415d002046cd13947fd698b80431a76cf4e
shahriarlabib000 <shahriarlabib000@gmail.com>
2024-07-26 17:02:38 +0600

    button label

--- scenes/airplane/airplane.gd
+++ scenes/airplane/airplane.gd
@@ -1,10 +1,12 @@
 extends RigidBody3D
 
+var engine_on:bool=true
 var turnSpeed:int=1500
 var RollSpeed:int=900
 var dir:float
 func _integrate_forces(_state: PhysicsDirectBodyState3D) -> void:
-	apply_central_force(basis.z * -200)
+	if engine_on:
+		apply_central_force(basis.z * -200)
 	
 func _physics_process(delta: float) -> void:
 	dir=Input.get_axis("ui_right","ui_left")
@@ -13,4 +15,11 @@ func _physics_process(delta: float) -> void:
 	apply_torque(basis.x * delta * dir * turnSpeed)
 	dir=Input.get_axis("rRight","rLeft")
 	apply_torque(basis.z * delta * dir * RollSpeed)
+	if Input.is_action_just_pressed("engine"):
+		engine_on=!engine_on
+		if engine_on:
+			gravity_scale=0
+		else :
+			gravity_scale=5
+			
 	
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -5,5 +5,5 @@ func _process(_delta: float) -> void:
 		$Camera3D.current=!$Camera3D.current
 	$Airplane/Camera3D.global_rotation.z=0
 	$Camera3D.look_at($Airplane.position)
-	$Camera3D.fov=(50/$Camera3D.position.distance_to($Airplane.position)*50)
+	$Camera3D.fov=30#(50/$Camera3D.position.distance_to($Airplane.position)*50)
 	$Camera3D.fov=clamp($Camera3D.fov,1,70)
